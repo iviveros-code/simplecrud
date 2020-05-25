43635f67330263b6ffd23f3f31e0c3a3d2f35d80
Ivan Viveros <bam@MacBook-Pro-de-Ivan.local>
2020-05-25 19:24:38 -0300

    actualice carpeta docs para hacer deploy

---
+++ src/docs/build/precache-manifest.6d2b5d2e78d8b3f402cf3611a07c77f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4641337e16c61d758fd993b6485fd16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f985dc5a1fe60d59d6c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9f33dca4ef2e2a5e008",
+    "url": "/static/js/2.ea11484c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ea11484c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f985dc5a1fe60d59d6c",
+    "url": "/static/js/main.97b17c46.chunk.js"
+  },
+  {
+    "revision": "7bf362e7762a86fd3c6d",
+    "url": "/static/js/runtime-main.6fde6a05.js"
+  }
+]);
