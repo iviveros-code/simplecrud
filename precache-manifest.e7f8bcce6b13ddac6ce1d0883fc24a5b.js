e0fe4d5777e63bd2f163f2f2740b3db7a71bf3dc
Ivan Viveros <bam@MacBook-Pro-de-Ivan.local>
2020-05-25 20:36:30 -0300

    Updates

---
+++ precache-manifest.e7f8bcce6b13ddac6ce1d0883fc24a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e25b0d001111ea3901193cefa02179",
+    "url": "/simplecrud/index.html"
+  },
+  {
+    "revision": "7f985dc5a1fe60d59d6c",
+    "url": "/simplecrud/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9f33dca4ef2e2a5e008",
+    "url": "/simplecrud/static/js/2.ea11484c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simplecrud/static/js/2.ea11484c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f985dc5a1fe60d59d6c",
+    "url": "/simplecrud/static/js/main.97b17c46.chunk.js"
+  },
+  {
+    "revision": "b391b28a2da59db27abb",
+    "url": "/simplecrud/static/js/runtime-main.059e160f.js"
+  }
+]);
